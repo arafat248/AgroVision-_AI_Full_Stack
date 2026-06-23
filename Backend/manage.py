51f507ca417c9dccb70a4334b286271b0480ac00
arafat248 <aab.arafat248@gmail.com>
2026-06-23 22:47:15 +0600

    complete backend site

---
+++ Backend/manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+from pathlib import Path
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent
+
+
+def main():
+    """Run administrative tasks."""
+    env = environ.Env()
+    env.read_env(os.path.join(BASE_DIR, '.env'))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', env('DJANGO_SETTINGS_MODULE', default='AgroVision.settings.local'))
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
